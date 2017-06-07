af72a327bb5216694432b41c82aef78eae0ff44f
Adam Faur <adam.faur@gmail.com>
2017-06-06 22:17:12 -0700

    Magic setters that add params through chain.

--- src/app/model/Person.php
+++ src/app/model/Person.php
@@ -4,14 +4,25 @@ use Mixin\AOP as AOP;
 
 class Person {
   use AOP;
-  public function address($location) {
+  public function __call($method, $args) {
+    $this->applyFilter(['details', function($SPC) use ($method, $args) {
+      try {
+        list($self, $params, $chain) = $SPC;
+        $params[$method] = $args[0];
+        $chain->next([$self, $params, $chain]);
+        return 'Added '.$method;
+      } catch (Exception $e) { echo 'Exception Thrown'; }
+    }]);
+  }
+  public function details() {
     $fnName   = __FUNCTION__;
-    $params   = compact('location');
+    $params   = [];
     $callback = function($SPC) {
       list($self, $params, $chain) = $SPC;
       extract($params);
       echo "\n";
       echo 'Hello '. $name ."\n";
+      echo 'Your age is '. $age ."\n";
       echo 'Your currently located in '. $location ."\n";
     };
     return $this->_filter([$fnName, $params, $callback]);
